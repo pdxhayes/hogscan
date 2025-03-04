11bd19733ae688a82aeaba2e58ef4489bc19b5b0
Russ Hayes <russ@hogscan.com>
2025-03-04 02:45:31 -0800

    First multisites

---
+++ web/sites/sites.php
@@ -0,0 +1,8 @@
+<?php
+
+$sites = array();
+
+$sites['0000.hogchapters.local']  = '0000.hogscan.com'; // template chapter
+$sites['0000.hogchapters.com']    = '0000.hogscan.com'; // template chapter
+$sites['9999.hogchapters.com']    = '9999.hogscan.com'; // demo chapter
+$sites['9999.hogchapters.local']  = '9999.hogscan.com'; // demo chapter
